0db479b29a813aae5cb50edb9777d2d53c8dcbde
andrew malikov <andrew.devels@gmail.com>
2018-03-07 22:52:34 +0500

    Remove deprecated methods from the Line module

--- src/line.py
+++ src/line.py
@@ -8,22 +8,6 @@ class Line():
         self.b = b
         self.c = c
 
-    # TODO: DEPRECATED
-    @staticmethod
-    def new_from_points(p1: Point, p2: Point):
-        a = p1.y - p2.y
-        b = p2.x - p1.x
-        c = -b * p1.y - a * p1.x
-        return Line(a, b, c)
-
-    # TODO: DEPRECATED
-    @staticmethod
-    def new_from_function(k: int, m: int):
-        a = k
-        b = -1
-        c = m
-        return Line(a, b, c)
-
     @staticmethod
     def is_parallel(l1, l2):
         return l1.a * l2.b == l2.a * l1.b
